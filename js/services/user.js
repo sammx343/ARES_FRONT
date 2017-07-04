28b9c30317d92283cdf2ff5984f850b61ee12b3e
Sebastian Mayor <naitsabes29@gmail.com>
2017-07-04 00:38:20 -0500

    changed url and stuff

--- js/services/user.js
+++ js/services/user.js
@@ -2,7 +2,7 @@ angular.module('ares')
 .factory('User', ['$http', function TopFactory($http){
   return {
     List: function(){
-      return $http({method: 'GET', url: 'http://ares-sammx343.c9users.io:8080/api/v1/users'})
+      return $http({method: 'GET', url: 'http://ares-uninorte.herokuapp.com/api/v1/users'})
     },
     Get : function(id, invites){
       return invites.filter(function(invited){
