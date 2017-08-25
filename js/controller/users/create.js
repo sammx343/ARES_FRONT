cdad44bc4c94f0e59ade62594ee2599b9771cac4
Sebastian Mayor <naitsabes29@gmail.com>
2017-08-25 17:06:31 -0500

    created user post

--- js/controller/users/create.js
+++ js/controller/users/create.js
@@ -1,8 +1,16 @@
 angular.module('ares')
 .controller("UserCreateController", ['$scope', '$route' ,'$routeParams', '$location', 'User', 
 function($scope, $route ,$routeParams, $location, Event){
-  $scope.id_type = ["CC", "TI", "PASAPORTE", "CC EXTRANJERÍA", "OTRO"]
+  $scope.id_type = ["CC", "TI", "PASAPORTE", "CC EXTRANJERÍA", "OTRO"];
   $scope.user = {};
+  $scope.rolIsAdmin = false;
+  $scope.roles = ["invitado", "admin"];
 
-  
+  $scope.selectedItemChanged = function(){
+    if($scope.user.role == "admin"){
+      $scope.rolIsAdmin = true;
+    }else{
+      $scope.rolIsAdmin = false;
+    }
+  }
 }]);
