6022067dd76f3df7c67c3455138a23105a883758
Sebastian Mayor <naitsabes29@gmail.com>
2017-09-05 18:22:13 -0500

    new checkbox to add users

--- js/controller/events/users.js
+++ js/controller/events/users.js
@@ -4,12 +4,23 @@ function($scope, $route ,$routeParams, $location, Event, User, NgTableParams){
 
   $scope.invites = [];
   $scope.event = {};
+  $scope.usersToAdd = [];
 
   $scope.add = function(event){
     Event.Create(event);
     console.log(event); 
     $location.path("/events");
     $route.reload();
+  }
+
+  $scope.addUsers = function(user){
+    var index = $scope.usersToAdd.indexOf(user.identification);
+    if(index == -1){
+      $scope.usersToAdd.push(user);
+    }else{
+      $scope.usersToAdd.splice(index, 1);
+    }
+    console.log($scope.usersToAdd);
   }
 
   User.List().then(function(data){
