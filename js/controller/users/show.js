19022d87af4a631b045aab48fe4c6d2d173071fc
Sebastian Mayor Molinares <smayor@redsis.com>
2017-09-28 16:58:15 -0500

    fix user delete and redirection

--- js/controller/users/show.js
+++ js/controller/users/show.js
@@ -1,6 +1,6 @@
 angular.module('ares')
-.controller("UserShowController", ['$scope', '$routeParams', 'User', 'Activity',
-  function($scope, $routeParams, User, Activity){
+.controller("UserShowController", ['$scope', '$route', '$routeParams', '$location', 'User', 'Activity',
+  function($scope, $route, $routeParams, $location, User, Activity){
 
   $scope.invited = {};
   $scope.activities = [];
@@ -12,7 +12,13 @@ angular.module('ares')
   });
 
   $scope.deleteUser = function(){
-    console.log("message");
-    
+    User.Delete($scope.invited.id).then(function(data){
+        swal({title: "Borrado", 
+              text: "El usuario ha sido borrado", 
+              type: "success"
+        }); 
+        $location.path("/events");
+        $route.reload();
+    });
   }
 }]);
